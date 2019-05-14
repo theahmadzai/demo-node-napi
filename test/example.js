89d36e7fc3d6536cb093cde0b51a2c9fd3317f66
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-05-14 17:56:14 +0500

    update tests

--- test/example.js
+++ test/example.js
@@ -1,10 +1,27 @@
-var example_c = require('bindings')('example_c');
-var example_cc = require('bindings')('example_cc');
+const example_c = require('../lib/example_c.js');
+const example_cc = require('../lib/example_cc.js');
+const assert = require('assert');
 
-console.log(example_c.add(3, 2));
+/**
+ * napi <node_api.h>
+ * example_c tests
+ */
+(function() {
+    assert.strictEqual(example_c.add(3, 3), 6, 'Unexpected Result: 6');
 
-example_c.test((arg) => {
-    console.log(arg)
-});
+    example_c.test((arg) => {
+        assert.strictEqual(arg, 'Hello World!', 'Unexpected Result: Hello World!');
+    });
 
-console.log(example_cc.hello());
+    console.log('example_c: All tests passed!');
+})();
+
+/**
+ * napi <napi.h>
+ * example_cc tests
+ */
+(function() {
+    assert.strictEqual(example_cc.hello(), 'world', 'Unexpected Result: world');
+
+    console.log('example_cc: All tests passed!');
+})();
